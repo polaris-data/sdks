5acfcef836a229edc15ea8ba472fd239118ba91d
Hilliam T. <hilliamtung@gmail.com>
2026-08-12 20:13:24 +0100

    Revert "Bump SDK minor versions"
    This reverts commit d43cf3ed5cee204d7f7e15eb6312bb60b28e9095.

--- typescript/src/client.ts
+++ typescript/src/client.ts
@@ -64,7 +64,7 @@ import { OrderbookBuilder } from "./orderbook";
 // SDK version – bumped manually during releases
 // ---------------------------------------------------------------------------
 
-const VERSION = "0.7.0";
+const VERSION = "0.6.0";
 
 // ---------------------------------------------------------------------------
 // Internal shorthand
